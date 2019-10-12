48eaad8682698074286d61a29ab7885b72d60702
xiaowei <469753862@qq.com>
2019-10-12 12:39:35 +0800

    Validator, 补充

--- code/validator/src/main/java/com/galaxy/validator/bean/FOO.java
+++ code/validator/src/main/java/com/galaxy/validator/bean/FOO.java
@@ -3,8 +3,6 @@ package com.galaxy.validator.bean;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NegativeOrZero;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -19,19 +17,25 @@ public class FOO {
     @NotNull(message = "id不能为空")
     private String id;
 
-    @NotBlank
+    @NotBlank(groups = NameNotBlank.class)
     @Size(min = 1, max = 50)
     @Pattern(regexp = "^[\\u4e00-\\u9fa5]*$")
     private String name;
 
     @NotBlank
-    @Size(min = 1, max = 15)
+    @Size(min = 1, max = 15, groups = ContactInfo.class)
     private String phone;
 
-    @NotBlank
-    @Email
+    @NotBlank(groups = ContactInfo.class)
+    @Email(groups = ContactInfo.class)
     private String email;
 
     private Integer age;
+
+    public interface ContactInfo {
+    }
+
+    public interface NameNotBlank {
+    }
 
 }
--- code/validator/src/main/java/com/galaxy/validator/controller/HelloController.java
+++ code/validator/src/main/java/com/galaxy/validator/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.galaxy.validator.controller;
 
 import com.galaxy.validator.bean.FOO;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Valid;
+import javax.validation.Validator;
+import java.util.Set;
 
 /**
  * @author galaxy
@@ -16,8 +22,66 @@ import javax.validation.Valid;
 @RequestMapping("hello")
 public class HelloController {
 
-    @PostMapping
-    public FOO hello(@Valid @RequestBody FOO foo) {
+    private final Validator validator;
+
+    public HelloController(Validator validator) {
+        this.validator = validator;
+    }
+
+
+    /** Valid JSR303 Bean Validation规范的支持,支持级联校验 */
+    @PostMapping("valid")
+    public FOO helloValid(@Valid @RequestBody FOO foo) {
+        return foo;
+    }
+
+    /** Validated Spring封装 Bean Validation 支持校验分组 */
+    @PostMapping("validated")
+    public FOO helloValidated(@Validated @RequestBody FOO foo) {
+
+        return foo;
+    }
+
+    /**
+     * validator 可以手动校验, 分别有两个接口类
+     * javax.validation.Validator和org.springframework.validation.Validator
+     * 因为使用spring的校验器, 我们这里就是用org.springframework.validation.Validator的实现类
+     */
+    @PostMapping("validator")
+    public FOO helloValidator(@RequestBody FOO foo) {
+        Set<ConstraintViolation<FOO>> constraintViolationSet = validator.validate(foo);
+        for (ConstraintViolation<FOO> fooConstraintViolation : constraintViolationSet) {
+            fooConstraintViolation.getMessage();
+            // todo galaxy 抛出业务错误信息
+        }
+        return foo;
+    }
+
+    /** BindingResult用于获取获取Validated和Valid的校验信息, 需要与Validated和Valid一一对应 */
+    @PostMapping("validatedBindingResult")
+    public FOO helloValidatedBindingResult(@Validated @RequestBody FOO foo, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                // todo galaxy 针对单个参数的异常抛出
+            }
+        }
+        return foo;
+    }
+
+    /** BindingResult用于获取获取Validated和Valid的校验信息, 需要与Validated和Valid一一对应 */
+    @PostMapping("validBindingResult")
+    public FOO helloValidBindingResult(@Valid @RequestBody FOO foo, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                // todo galaxy 针对单个参数的异常抛出
+            }
+        }
+        return foo;
+    }
+
+    /** Validated提供校验分组,根据不同的业务进行分组 */
+    @PostMapping("groups")
+    public FOO helloGroups(@Validated(value = FOO.NameNotBlank.class) @RequestBody FOO foo) {
         return foo;
     }
 }
