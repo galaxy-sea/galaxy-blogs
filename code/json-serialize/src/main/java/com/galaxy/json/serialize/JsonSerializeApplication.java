0f7bcd8d589f66fd09123997d93ab7d445291450
galaxy <469753862@qq.com>
2019-11-18 10:44:06 +0800

    json序列化问题

---
+++ code/json-serialize/src/main/java/com/galaxy/json/serialize/JsonSerializeApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.json.serialize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSerializeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonSerializeApplication.class, args);
+    }
+
+}
---
+++ code/json-serialize/src/test/java/com/galaxy/json/serialize/JsonSerializeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.json.serialize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonSerializeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
