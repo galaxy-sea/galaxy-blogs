8d4f2dd0d8159d9a6fdf43a73f41a6c72703d32e
galaxy <469753862@qq.com>
2019-12-03 11:29:08 +0800

    日了狗了

--- code/json-serialize/jackson/src/test/java/com/galaxy/jackson/JacksonApplicationTests.java
+++ code/json-serialize/jackson/src/test/java/com/galaxy/jackson/JacksonApplicationTests.java
@@ -2,11 +2,8 @@ package com.galaxy.jackson;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.Version;
-import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.galaxy.jackson.bean.Panda;
 import org.junit.jupiter.api.Test;
 
@@ -47,145 +44,7 @@ class JacksonApplicationTests {
 
     @Test
     public void JsonDeserializer() throws JsonProcessingException {
-        Panda panda = objectMapper.readValue("{\"phone\":\"前缀132123\",\"name\":null,\"age\":17,\"nameList\":[\"前缀1\",\"前缀2\",\"前缀3\",\"前缀4\"]}", Panda.class);
-
-        objectMapper.registerModule(new SimpleModule());
-
-
-        objectMapper.registerModule(new Module() {
-            @Override
-            public String getModuleName() {
-                return null;
-            }
-
-            @Override
-            public Version version() {
-                return null;
-            }
-
-            @Override
-            public void setupModule(SetupContext context) {
-
-            }
-        });
-
-
+        Panda panda = objectMapper.readValue("{\"phone\":\"前缀132123\",\"name\":null,\"age\":17,\"nameList\":[\"前缀1\",null,\"前缀3\",\"前缀4\"]}", Panda.class);
         System.out.println(panda);
     }
-
-
-    /**
-     * Assume you have a custom collection.
-     */
-    @JsonDeserialize(as = ImmutableBag.class)
-    public interface Bag<T> extends Collection<T> {
-
-    }
-
-    /**
-     * It is deserialized as an immutable version (i.e. no add / remove, etc.).
-     */
-    public static class ImmutableBag<T> extends AbstractCollection<T> implements Bag<T> {
-        @Override
-        public Iterator<T> iterator() {
-            return elements.iterator();
-        }
-
-        @Override
-        public int size() {
-            return elements.size();
-        }
-
-        @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-        private ImmutableBag(Collection<T> elements) {
-            this.elements = Collections.unmodifiableCollection(elements);
-        }
-
-        private final Collection<T> elements;
-    }
-
-    public static class Value {
-        public Value(String value) {
-            this.value = value;
-        }
-
-        private String value;
-    }
-
-    /**
-     * This does not work - see {@link #testDeserializeBagOfStrings}.
-     */
-    public static class WithBagOfStrings {
-
-        public Bag<String> getStrings() {
-            return this.bagOfStrings;
-        }
-
-        public void setStrings(Bag<String> bagOfStrings) {
-            this.bagOfStrings = bagOfStrings;
-        }
-
-        private Bag<String> bagOfStrings;
-    }
-
-    /**
-     * This DOES work - see {@link #testDeserializeBagOfObjects}.
-     */
-    public static class WithBagOfValues {
-        public Bag<Value> getValues() {
-            return this.bagOfValues;
-        }
-
-        public void setValues(Bag<Value> bagOfValues) {
-            this.bagOfValues = bagOfValues;
-        }
-
-        private Bag<Value> bagOfValues;
-    }
-
-
-    @Test
-    public void testDeserializeBagOfStrings() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        WithBagOfStrings result = mapper.readerFor(WithBagOfStrings.class)
-                .readValue("{\"strings\": [ \"a\", \"b\", \"c\"]}");
-        // Fails with:
-        // com.fasterxml.jackson.databind.exc.MismatchedInputException: Cannot construct instance of
-        // `...$ImmutableBag` (although at least one Creator exists): no default no-arguments constructor found
-        // at [Source: (String)"{"strings": [ "a", "b", "c"]}"; line: 1, column: 13] (through reference chain:
-        // ...$WithBagOfStrings["strings"])
-        //
-        //	at com.fasterxml.jackson.databind.exc.MismatchedInputException.from(MismatchedInputException.java:63)
-        //	at com.fasterxml.jackson.databind.DeserializationContext.reportInputMismatch(DeserializationContext.java:1343)
-        //	at com.fasterxml.jackson.databind.DeserializationContext.handleMissingInstantiator(DeserializationContext.java:1032)
-        //	at com.fasterxml.jackson.databind.deser.ValueInstantiator.createUsingDefault(ValueInstantiator.java:189)
-        //	at com.fasterxml.jackson.databind.deser.std.StdValueInstantiator.createUsingDefault(StdValueInstantiator.java:267)
-        //	at com.fasterxml.jackson.databind.deser.std.StringCollectionDeserializer.deserialize(StringCollectionDeserializer.java:168)
-        //	at com.fasterxml.jackson.databind.deser.std.StringCollectionDeserializer.deserialize(StringCollectionDeserializer.java:21)
-        //	at com.fasterxml.jackson.databind.deser.impl.MethodProperty.deserializeAndSet(MethodProperty.java:127)
-        //	at com.fasterxml.jackson.databind.deser.BeanDeserializer.vanillaDeserialize(BeanDeserializer.java:288)
-        //	at com.fasterxml.jackson.databind.deser.BeanDeserializer.deserialize(BeanDeserializer.java:151)
-        //	at com.fasterxml.jackson.databind.ObjectReader._bindAndClose(ObjectReader.java:1611)
-        //	at com.fasterxml.jackson.databind.ObjectReader.readValue(ObjectReader.java:1219)
-        //        Assert.assertEquals(3, result.getStrings().size());
-    }
-
-    @Test
-    public void testDeserializeBagOfObjects() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        WithBagOfValues result = mapper.readerFor(WithBagOfValues.class)
-                .readValue("{\"values\": [ \"a\", \"b\", \"c\"]}");
-        //        Assert.assertEquals(3, result.getValues().size());
-    }
-
-    @Test
-    public void testBagOfStringsAlone() throws IOException {
-        // curiously, this DOES work too!
-        ObjectMapper mapper = new ObjectMapper();
-        Bag<String> result = mapper.readerFor(Bag.class)
-                .readValue("[ \"a\", \"b\", \"c\"]");
-        //        Assert.assertEquals(3, result.size());
-    }
-
-
 }
