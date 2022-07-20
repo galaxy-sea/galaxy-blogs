58e4c10c7ac2f786e5b09dac36b62cd7c4f554e7
galaxy <469753862@qq.com>
2022-07-20 16:38:26 +0800

    ddns add buuntu

--- shell/linux/ddnsAndSendEmail.sh
+++ shell/linux/ddnsAndSendEmail.sh
@@ -10,7 +10,7 @@
 
 Obtain_IP_ident="curl -s myip.ipip.net/s"
 Obtain_IP_myip="curl -s ip.3322.net"
-Old_LocalIP=$(head -n +1 /root/Old_LocalIP.txt)
+Old_LocalIP=$(head -n +1 /home/changjinwei/Old_LocalIP.txt)
 
 # 第一次检查IP
 New_LocalIP=`${Obtain_IP_ident}`
@@ -18,11 +18,29 @@ if [ "${Old_LocalIP}" != "${New_LocalIP}" ]
 then
     # 第二次检查IP
     check_ip=`${Obtain_IP_myip}`
-    if [ "${check_ip}" == "${New_LocalIP}" ]
+    if [ "${check_ip}" = "${New_LocalIP}" ]
     then
         Old_LocalIP=${New_LocalIP}
-        /usr/local/bin/aliyun alidns UpdateDomainRecord --region cn-hangzhou --RecordId 770712821778851840 --RR *.nuc8i7 --Type A --Value "${New_LocalIP}"
-        echo "${New_LocalIP}" | mail -s "nuc8i7 ip change" 469753862@qq.com
+        # aliyun ddns
+        # /usr/local/bin/aliyun alidns UpdateDomainRecord --region cn-hangzhou --RecordId 770712821778851840 --RR *.nuc8i7 --Type A --Value "${New_LocalIP}"
+
+        # centOS send mail
+        # echo "${New_LocalIP}" | mail -s "nuc8i7 ip change" 469753862@qq.com
+
+        # ubuntu send mail
+        echo "${New_LocalIP}" | s-nail -s "nuc8i7 ip change" 469753862@qq.com
+        
+        # write ip
+        echo "${New_LocalIP}" > /home/changjinwei/Old_LocalIP.txt
     fi
 fi
-echo "${New_LocalIP}" > /root/Old_LocalIP.txt
+
+
+# read -p "please input a number: " number
+
+# if [[ $number =~ ^([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$ ]];then
+#     echo "correct"
+# else
+#     echo "error"
+# fi
+
