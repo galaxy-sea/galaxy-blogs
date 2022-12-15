ee6ff5ddb9854422dea137c8bd54157407b5c0a5
小魏,小魏,我们要去哪里呀 <weichangjin1996@gmail.com>
2022-12-15 22:53:57 +0800

    增加 author

--- code/security-annotation/src/main/java/plus/wcj/security/annotation/SecurityAnnotationApplication.java
+++ code/security-annotation/src/main/java/plus/wcj/security/annotation/SecurityAnnotationApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+/**
+ * @author changjin wei(魏昌进)
+ * @since 2022/12/15
+ */
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityAnnotationApplication {
